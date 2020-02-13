984ba50e4a9fa02d3ed9b41b0b611c75024ad498
Suresh <Suresh@LAPTOP-TATKDIL6>
2020-02-13 10:21:32 +0530

    update

--- ANZ-Wholesale-App/src/main/java/com/anz/wholesale/controller/AccountController.java
+++ ANZ-Wholesale-App/src/main/java/com/anz/wholesale/controller/AccountController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anz.wholesale.bean.AccountList;
@@ -29,14 +31,14 @@ public class AccountController {
 
 	}
 
-@RequestMapping(value = "/accountList", method = RequestMethod.GET,consumes = "application/json", produces = "application/json")
+@RequestMapping(value = "/accountList", method = RequestMethod.GET)
 public List<AccountList> getAllAccountList() {
 	logger.debug("Inside AccountList Controller Method ");
 	return accountService.getAllAccountList();
 }
 
-@RequestMapping(value = "/transcationList", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
-public List<AccountTranscation> getAlltranscationList(@RequestBody String accountNumber) {
+@RequestMapping(value = "/transcationList", method = RequestMethod.GET)
+public List<AccountTranscation> getAlltranscationList(@RequestParam("accountNumber") String accountNumber) {
 	logger.debug("Inside TranscationList Controller Method ");
 	return accountService.getAllTranscationList(accountNumber);
 }
